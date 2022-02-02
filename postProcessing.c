2249227682f08072db98b949552e4a704cd66c23
Thomas Pinkava <pinkavaprime@gmail.com>
2022-02-01 22:02:31 -0800

    temporary backup for minor sheathing overhaul

--- postProcessing.c
+++ postProcessing.c
@@ -5,7 +5,7 @@
 // See header for details
 
 
-/* TODO: under consideration for replacement with general isolation fill
+///TODO: under consideration for replacement with general isolation fill
 // THINFILLING STEP
 // TODO document better
 // TODO migrate to own file?
@@ -52,7 +52,7 @@ void thinFillingStep(struct ascoTileMap *map){
     freeCoordQueue(queue);
 }
 
-*/
+
 
 
 
@@ -159,7 +159,6 @@ void cliffOozeStep(struct ascoTileMap *map){
                 uint8_t cMS = MSFromVariantRotation[c->variant][c->rotation];
                 uint8_t dMS = MSFromVariantRotation[d->variant][d->rotation];
 
-                //printf("(%d, %d): %d %d %d %d\n", x, y, aMS, bMS, cMS, dMS); // TODO debug
                 
                 // Set the Marching squares height corner at the center of the region to 1
                 aMS = aMS | 2;
@@ -216,6 +215,7 @@ void cliffOozeStep(struct ascoTileMap *map){
 
 
 
+// Helper macro for inaccessibleCliffStep
 #define TILE_MAKES_INACCESSIBLE(t) ((t) == TILE_CLIFF || (t) == TILE_BLOCKAGE)
 
 
@@ -283,6 +283,199 @@ void inaccessibleCliffStep(struct ascoTileMap *map){
 
 
 
+/* TODO doesn't really work
+// TODO document 
+// turns unresolveds to blockage at random based on their neighbors
+void rockSprinklingStep(struct ascoTileMap *map){
+    // Set up a coord queue
+    struct coordQueue *queue = newCoordQueue(16);    // TODO initial size...?
+
+    for(int y = 0; y < map->height; y++){
+        for(int x = 0; x < map->width; x++){
+            if(mapCell(map, x, y).tile == TILE_UNRESOLVED){
+                // If we flatten to blockage, we enqueue our neighbors; therefore start by enqueueing
+                enCoordQueue(queue, x, y);
+
+                int m, n;
+                while(deCoordQueue(queue, &m, &n)){
+
+                    // Count neighbor blockages: blockages are worth 2, unresolveds worth 1, blanks worth zero.
+                    int neighborValue = 0;
+                    if(n > 0) neighborValue += mapCell(map, m, n-1).tile == TILE_BLANK ? 0 : mapCell(map, m, n-1).tile == TILE_UNRESOLVED ? 1 : 2;
+                    if(m < map->width - 1) neighborValue += mapCell(map, m+1, n).tile == TILE_BLANK ? 0 : mapCell(map, m+1, n).tile == TILE_UNRESOLVED ? 1 : 2;
+                    if(n < map->height - 1) neighborValue += mapCell(map, m, n+1).tile == TILE_BLANK ? 0 : mapCell(map, m, n+1).tile == TILE_UNRESOLVED ? 1 : 2;
+                    if(m > 0) neighborValue += mapCell(map, m-1, n).tile == TILE_BLANK ? 0 : mapCell(map, m-1, n).tile == TILE_UNRESOLVED ? 1 : 2;
+
+                    // neighborValue should now lie between zero and eight; set the decision threshold accordingly
+                    // TODO for now it's linear; probably not the best
+                    int threshold = 5 * neighborValue;
+                    
+                    if(rand() % 80 < threshold){
+                        mapCell(map, m, n).tile = TILE_BLOCKAGE;
+
+                        // ...and enqueue neighbor UNRESOLVEDs
+                        if(n > 0 && mapCell(map, m, n-1).tile == TILE_UNRESOLVED) enCoordQueue(queue, m, n-1);
+                        if(m < map->width-1 && mapCell(map, m+1, n).tile == TILE_UNRESOLVED) enCoordQueue(queue, m+1, n);
+                        if(n < map->height-1 && mapCell(map, m, n+1).tile == TILE_UNRESOLVED) enCoordQueue(queue, m, n+1);
+                        if(m > 0 && mapCell(map, m-1, n).tile == TILE_UNRESOLVED) enCoordQueue(queue, m-1, n);
+                        
+                    }
+
+                }
+            }
+        }
+    }
+
+    freeCoordQueue(queue);
+}
+*/
+
+
+// Helper macro for isValidRockSite
+#define TILE_IS_FLOOR(t) ((t) == TILE_BLANK || (t) == TILE_UNRESOLVED)
+
+// Code ported from AscoSpel v1 (I've forgotten how it works, which is a pity, because it's really cool)
+// Returns truth if
+//  a) a blockage tile can be legitimately placed at (x, y)
+//  b) placing a rock tile at (x, y) would leave the map topology unaltered (i.e. wouldn't block a path)
+//  returns falsehood otherwise
+static unsigned int isValidRockSite(struct ascoTileMap *map, int x, int y){
+
+    // Initial assumption: blockage can't be placed on the map border (accelerates neighbor checks by removing bound checks)
+    // Also check if the site is UNRESOLVED
+    // TODO make function precondition for traversal (save checks all)
+    if(x <= 0 || x >= map->width || y <= 0 || y >= map->height || mapCell(map, x, y).tile != TILE_UNRESOLVED) return 0;
+
+
+    // Gather together the Von Neumann neigborhood of the cell
+    unsigned int neighborSet = 0;
+
+    if(TILE_IS_FLOOR(mapCell(map, x, y-1).tile)) neighborSet |= 1;
+    if(TILE_IS_FLOOR(mapCell(map, x+1, y).tile)) neighborSet |= 2;
+    if(TILE_IS_FLOOR(mapCell(map, x, y+1).tile)) neighborSet |= 4;
+    if(TILE_IS_FLOOR(mapCell(map, x-1, y).tile)) neighborSet |= 8;
+
+    // Edge case checking (path going straight through)
+    if(neighborSet == 5 || neighborSet == 10) return 0;
+
+    // AND the neighbor set with its right-rotation to determine if the putative rock will block access to anything
+    // (Copied from prototype -- I've lost the notes on how this works)
+    neighborSet = (neighborSet & (neighborSet >> 1 | ((neighborSet & 1) ? 8 : 0)));
+
+    unsigned int cornerSet = 0;
+    cornerSet |= (TILE_IS_FLOOR(mapCell(map, x+1, y-1).tile)) ? 1 : 0; 
+    cornerSet |= (TILE_IS_FLOOR(mapCell(map, x+1, y+1).tile)) ? 2 : 0; 
+    cornerSet |= (TILE_IS_FLOOR(mapCell(map, x-1, y+1).tile)) ? 4 : 0; 
+    cornerSet |= (TILE_IS_FLOOR(mapCell(map, x-1, y-1).tile)) ? 8 : 0;
+
+    if((neighborSet & cornerSet) == neighborSet) return 1;
+
+    return 0;
+}
+
+
+
+
+// TODO document
+void fenceBreakingStep(struct ascoTileMap *map){
+    // Pass over twice to ensure fences are fully broken
+    for(int times = 0; times < 2; times++){
+
+        for(int y = 1; y < map->height - 1; y++){
+            for(int x = 1; x < map->width - 1; x++){
+                if(mapCell(map, x, y).tile == TILE_BLOCKAGE){
+                    // If the cell is blockage, check to see if we can remove it
+                    
+                    // 1) Gather together Von Neumann neighborhood, clockwise from top.
+                    unsigned int neighborSet = 0;
+
+                    if(TILE_IS_FLOOR(mapCell(map, x, y-1).tile)) neighborSet |= 1;
+                    if(TILE_IS_FLOOR(mapCell(map, x+1, y).tile)) neighborSet |= 2;
+                    if(TILE_IS_FLOOR(mapCell(map, x, y+1).tile)) neighborSet |= 4;
+                    if(TILE_IS_FLOOR(mapCell(map, x-1, y).tile)) neighborSet |= 8;
+
+                    // 2) If VN neighborhood has only one floor, it's safe to flatten this blockage
+                    if(neighborSet == 1 || neighborSet == 2 || neighborSet == 4 || neighborSet == 8){   // TODO cheaper check?
+                        mapCell(map, x, y).tile = TILE_UNRESOLVED;
+                        continue;
+                    }
+
+                    /* 
+                    // 3) If VN neighborhood has three floors, we have to check corners.
+                    if(neighborSet == 7 || neighborSet == 11 || neighborSet == 13 || neighborSet == 14){    // TODO cheaper check?
+
+                        // If two floor sides are connected by a nonfloor corner, we can't flatten.
+                        // bitwise-AND the neighbor set with its right-rotation; the bits that are lit are the corners (clockwise from TR) that join two floor edges
+                        neighborSet = (neighborSet & (neighborSet >> 1 | ((neighborSet & 1) ? 8 : 0)));
+
+                        // Amalgamate the corners
+                        unsigned int cornerSet = 0;
+                        cornerSet |= (!TILE_IS_FLOOR(mapCell(map, x+1, y-1).tile)) ? 1 : 0; 
+                        cornerSet |= (!TILE_IS_FLOOR(mapCell(map, x+1, y+1).tile)) ? 2 : 0; 
+                        cornerSet |= (!TILE_IS_FLOOR(mapCell(map, x-1, y+1).tile)) ? 4 : 0; 
+                        cornerSet |= (!TILE_IS_FLOOR(mapCell(map, x-1, y-1).tile)) ? 8 : 0;
+
+                        // bitwise-AND corner set and neighbor set; the bits lit are corners joining two floors that are themselves not floors.
+                        // If any such exist, we cannot flatten.
+                        if(!(neighborSet & cornerSet)){
+                            mapCell(map, x, y).tile = TILE_UNRESOLVED;
+                        }
+                    } 
+                    */
+                    
+                }
+            }
+        }
+
+
+    }
+}
+
+
+
+
+
+void alternativeFenceBreakingStep(struct ascoTileMap *map){
+
+    // Iterate within a border to save on bounds-checking
+    for(int y = 1; y < map->height - 1; y++){
+        for(int x = 1; x < map->width - 1; x++){
+            if(mapCell(map, x, y).tile == TILE_BLOCKAGE){
+                
+                // TODO improve this logic
+                if(
+                    (mapCell(map, x, y-1).tile == TILE_CLIFF && mapCell(map, x+1, y).tile == TILE_BLOCKAGE &&
+                     TILE_IS_FLOOR(mapCell(map, x, y+1).tile) && mapCell(map, x-1, y).tile == TILE_BLOCKAGE)
+                    ||
+                    (mapCell(map, x, y-1).tile == TILE_BLOCKAGE && mapCell(map, x+1, y).tile == TILE_CLIFF &&
+                     mapCell(map, x, y+1).tile == TILE_BLOCKAGE && TILE_IS_FLOOR(mapCell(map, x-1, y).tile))
+                    ||
+                    (TILE_IS_FLOOR(mapCell(map, x, y-1).tile) && mapCell(map, x+1, y).tile == TILE_BLOCKAGE &&
+                     mapCell(map, x, y+1).tile == TILE_CLIFF && mapCell(map, x-1, y).tile == TILE_BLOCKAGE)
+                    ||
+                    (mapCell(map, x, y-1).tile == TILE_BLOCKAGE && TILE_IS_FLOOR(mapCell(map, x+1, y).tile) &&
+                     mapCell(map, x, y+1).tile == TILE_BLOCKAGE && mapCell(map, x-1, y).tile == TILE_CLIFF)
+                ){
+                    mapCell(map, x, y).variant = 1;
+                }
+
+            }
+        }
+    }
+
+    for(int i = 0; i < map->width * map->height; i++){
+        if(map->cells[i].tile == TILE_BLOCKAGE && map->cells[i].variant == 1){
+            map->cells[i].variant = 0;
+            map->cells[i].tile = TILE_UNRESOLVED;
+        }
+    }
+}
+
+
+
+
+
+
 void tempPostProcess(struct ascoTileMap *map){
     // 1) Thinfilling step
     //thinFillingStep(map);
@@ -303,8 +496,33 @@ void tempPostProcess(struct ascoTileMap *map){
         cliffOozeStep(map);  // TODO run as many times as the map is deep (or some fraction thereof?)
     }
 
+
+
+
+    // 4) Rock sprinkling
+    //rockSprinklingStep(map);
+    /*
+    for(int y = 0; y < map->height; y++){
+        for(int x = 0; x < map->width; x++){
+            if(isValidRockSite(map, x, y) && !(rand() % 4)) mapCell(map, x, y).tile = TILE_ROCK_SMALL;
+        }
+    }
+    */
+    //printAscoTileMap(map);
+    //fenceBreakingStep(map);
+
+
+    alternativeFenceBreakingStep(map);
+
+    // TODO test this
+    thinFillingStep(map);
+    cliffOozeStep(map);
+    cliffOozeStep(map);
+    alternativeFenceBreakingStep(map);
+
     // 3) Flatten blockages atop cliffs to inaccessible cliff
     inaccessibleCliffStep(map);
+
 
 
     // Temp large rock accretion step (only operating on oozed cliffs for now)
@@ -324,4 +542,11 @@ void tempPostProcess(struct ascoTileMap *map){
         }
     }
 
+    // One final solve pass (join with accretor)
+    /* 
+    for(int i = 0; i < map->width * map->height; i++){
+        if(map->cells[i].tile == TILE_UNRESOLVED) map->cells[i].tile = TILE_BLANK;
+        if(map->cells[i].tile == TILE_BLOCKAGE) map->cells[i].tile = (rand() % 2) ? TILE_ROCK_SMALL : TILE_ROCK_TALL;
+    }
+    */
 }
